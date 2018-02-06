c32f04a997abdbc8291974da397f1c931c2b744c
moustafa-mk <30719967+moustafa-mk@users.noreply.github.com>
2018-02-06 21:10:57 +0200

    Update FibNim.py
    Fibonacci Nim V1.0
    What's new:
    -Now players can enter their names!!
    -Fixed "computer can choose zero" bug
    
    Enjoy the game!! <3

--- FibNim.py
+++ FibNim.py
@@ -3,6 +3,8 @@ import random
 orig = random.randint(20,1000)
 last = (orig - 1) / 2
 beg = "false"
+p1Name = ""
+p2Name = ""
 
 def tryagain(lastuse, current):
     global beg
@@ -19,10 +21,11 @@ def tryagain(lastuse, current):
         return (ret)
 
 def player1():
+    global p1Name
     global last
     global orig
     global beg
-    p1 = int(input("Player one's turn: "))
+    p1 = int(input(p1Name + "'s turn: "))
     if p1 > 2*last or p1 > orig:
         p1 = tryagain(last, orig)
     last = p1
@@ -31,10 +34,11 @@ def player1():
     print ("There are " + str(orig) + " coins on the table")
 
 def player2():
+    global p2Name
     global last
     global orig
     global beg
-    p2 = int(input("Player two's turn: "))
+    p2 = int(input(p2Name + "'s turn: "))
     if p2 > 2*last or p2 > orig:
         p2 = tryagain(last, orig)
     last = p2
@@ -50,6 +54,8 @@ def comp():
         cplay = orig
     else:
         cplay = orig // 4
+        if cplay == 0:
+            cplay = 1
     print("Computer chose the number " + str(cplay))
     last = cplay
     orig -= last
@@ -67,32 +73,40 @@ def playagain():
         exit()
 
 def multiplayer():
+    global p1Name
+    global p2Name
     print ("There are " + str(orig) + " coins on the table")
     while orig != 0:
         player1()
         if orig == 0:
-            print("Player one wins!!")
+            print(p1Name + " wins!!")
             playagain()
         player2()
-    print("Player two wins!!")
+    print(p2Name + " wins!!")
     playagain()
 
 def oneplayer():
+    global p1Name
     print ("There are " + str(orig) + " coins on the table")
     while orig != 0:
         player1()
         if orig == 0:
-            print("Player one wins!!")
+            print(p1Name + " wins!!")
             playagain()
         comp()
     print("Computer wins!!")
     playagain()
 
 def game():
+    global p1Name
+    global p2Name
     playmode = input("Enter 1 for one player or 2 for two players: ")
     if playmode == "1":
+        p1Name = input("Player 1 name: ")
         oneplayer()
     else:
+        p1Name = input("Player 1 name: ")
+        p2Name = input("Player 2 name: ")
         multiplayer()
     
 game()
